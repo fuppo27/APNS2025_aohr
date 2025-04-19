bb8aa096e203b9bf15271099892e9f6bcda9764e
fsm <takayasu.fushimi@gmail.com>
2025-04-19 21:45:41 +0900

    gen_roadNWのバグ修正

--- gen_roadNW.py
+++ gen_roadNW.py
@@ -1,9 +1,11 @@
 import sys
 import osmnx as ox
+import networkx as nx
+
 
 city = sys.argv[1]  # 対象都市名：hachioji, yokohama
-pref = sys.argv[2]  # 都道府県名
-query = f"{city}shi, {pref}, Japan"  # "Hachiojishi, Tokyo, Japan"
+pref = sys.argv[2]  # 都道府県名：tokyo, kanagawa
+query = f"{city.capitalize()}, {pref.capitalize()}, Japan"  # "Hachioji, Tokyo, Japan"
 G = ox.graph_from_place(query, network_type = "drive")  #'drive', 'bike', 'walk'
 #start = [35.63292692012474, 139.88037284233025]  # 出発点の緯度経度
 #G = ox.graph_from_point(start, network_type = "walk", dist = 1200) #'drive', 'bike', 'walk'
@@ -19,4 +21,4 @@ ox.save_graphml(G, filepath=f"data/{city}/road_network.graphml")
 
 nodes = ox.graph_to_gdfs(G, nodes=True, edges=False).reset_index()
 nodes = nodes[["osmid", "y", "x"]]  # 必要なカラムだけをフィルタリングして取得
-nodes_save.to_csv(f"data/{city}/nodes.csv")
+nodes.to_csv(f"data/{city}/nodes.csv")
